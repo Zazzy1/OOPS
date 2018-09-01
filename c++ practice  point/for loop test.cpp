89a4fa123c703aaf088d86ebdf0a15bc69e513c3
BIVAS DUTTA <zazzydutta96@gmail.com>
2018-09-01 14:47:39 +0530

    Add files via upload

---
+++ c++ practice  point/for loop test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+   // for loop execution
+   for( int a = 10; a < 20; ) {
+      cout << "value of a: " << a << endl;
+       a = a + 1 ;
+   }
+
+   return 0;
+}
---
+++ c++ practice  point/infinite loop assumes condition to be true inside().cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+   for( ; ; ) {
+      cout<<"This loop will run forever.\n";
+   }
+
+   return 0;
+}
